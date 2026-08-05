f5829f03975ba61123667399828ac9b8f5a5f186
aizazahmad736 <aizazexforwardian@gmail.com>
2026-08-05 16:39:04 +0500

    initialized app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib
+
+# Load model
+model = joblib.load("house_price_model.pkl")
+
+st.title("🏠 House Price Prediction")
+
+st.write("Enter house details to predict the price")
+
+st.success("Model loaded successfully!")
