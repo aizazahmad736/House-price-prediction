992e54c5b077ddfe4f00e76432ba478e47f4fe88
aizazahmad736 <aizazexforwardian@gmail.com>
2026-08-05 16:43:45 +0500

    modeified app.py code

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-import streamlit as st
+# import streamlit as st
+# import joblib
+
+# # Load model
+# model = joblib.load("house_price_model.pkl")
+
+# st.title("🏠 House Price Prediction")
+
+# st.write("Enter house details to predict the price")
+
+# st.success("Model loaded successfully!")
+
 import joblib
 
-# Load model
+# Load trained model
 model = joblib.load("house_price_model.pkl")
 
-st.title("🏠 House Price Prediction")
 
-st.write("Enter house details to predict the price")
+def predict_price(features):
+    prediction = model.predict([features])
+    return prediction[0]
 
-st.success("Model loaded successfully!")
+
+print("Model loaded successfully")
